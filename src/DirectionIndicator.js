6538054a36fe638d6f3651aa62695e6ca923a75d
Senja Jarva <senja.jarva@netlight.com>
2019-07-12 16:47:07 +0300

    Fix CSS bug of DirectionIndicator not being very easy to click

--- src/DirectionIndicator.js
+++ src/DirectionIndicator.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const DirectionIndicator = ({c, r, direction, directionChange}) => {
-    return <div className={`grid-item-${c}-${r} grid-item__centered`} onClick={directionChange}>
-        {direction === 'forward' ? '/' : '\\'}
+    return <div className={`grid-item__centered grid-item-${c}-${r}`} onClick={directionChange}>
+        <div>{direction === 'forward' ? '/' : '\\'} </div>
     </div>;
 
 }
