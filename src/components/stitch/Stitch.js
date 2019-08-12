24a6a0b7f9d0c8041baede93b1524e8e5946acf3
Senja Jarva <senja@jarva.fi>
2019-08-12 15:49:04 +0300

    Removed showing the warp index number inside a stitch

--- src/components/stitch/Stitch.js
+++ src/components/stitch/Stitch.js
@@ -3,17 +3,16 @@ import './Stitch.scss';
 
 const Stitch = ({color, status, c, r, againstMasterDir, isDouble, number}) => {
         return <div className={`stitch stitch__${againstMasterDir ? 'against': 'along'}`}>
-            {isDouble && 
+            {isDouble &&
                 <div className={`oval oval__${status}`}>
                     <div className={`double-oval double-oval__${status}`} style={{backgroundColor: color}}>
-                        {number}
                     </div>
                 </div>
             }
 
-            {!isDouble && 
+            {!isDouble &&
             <div className={`oval oval__${status}`} style={{backgroundColor: color}}>
-                <div>{number}</div>
+
             </div>
             }
         </div>;
